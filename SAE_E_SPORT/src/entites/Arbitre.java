917b02cd2231b0d1621b06f5732802dd90c57aa5
Zelmine <zelmine31@gmail.com>
2023-11-13 09:40:16 +0100

    test

--- SAE_E_SPORT/src/entites/Arbitre.java
+++ SAE_E_SPORT/src/entites/Arbitre.java
@@ -1,57 +1,61 @@
 package entites;
 
 public class Arbitre {
-	
-	private int idArbitre;
-	private String nomArbitre;
-	private String prenomArbitre;
-	
-	public Arbitre(int idArbitre, String nomArbitre, String prenomArbitre) {
-		this.idArbitre = idArbitre;
-		this.nomArbitre = nomArbitre;
-		this.prenomArbitre = nomArbitre;
-	}
-	
-	public int getidArbitre() {
-		return this.idArbitre;
-	}
-	
-	public String getnomArbitre() {
-		return this.nomArbitre;
-	}
-	
-	public String getprenomArbitre() {
-		return this.prenomArbitre;
-	}
-	
-	public void setidArbitre(int idArbitre) {
-		this.idArbitre = idArbitre;
-	}
-	
-	public void setnomArbitre(String nomArbitre) {
-		this.nomArbitre = nomArbitre;
-	}
-	
-	public void setprenomArbitre(String prenomArbitre) {
-		this.prenomArbitre = prenomArbitre;
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if(o instanceof Arbitre) {
-			Arbitre a = (Arbitre) o;
-			if (this.idArbitre == a.getidArbitre()) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		return false;
-	}
-	
-	@Override
-	public String toString() {
-		return this.idArbitre + " - "+this.nomArbitre + " - "+this.prenomArbitre;
-	}	
+    
+    private int idArbitre;
+    private String nomArbitre;
+    private String prenomArbitre;
+    
+    
+    // Constructeur
+    public Arbitre(int idArbitre, String nomArbitre, String prenomArbitre) {
+        this.idArbitre = idArbitre;
+        this.nomArbitre = nomArbitre;
+        this.prenomArbitre = nomArbitre;
+    }
+    
+    
+    // Get
+    public int getId() {
+        return this.idArbitre;
+    }
+    
+    public String getNom() {
+        return this.nomArbitre;
+    }
+    
+    public String getPrenom() {
+        return this.prenomArbitre;
+    }
+    
+    
+    // Set
+    public void setNom(String nomArbitre) {
+        this.nomArbitre = nomArbitre;
+    }
+    
+    public void setPrenom(String prenomArbitre) {
+        this.prenomArbitre = prenomArbitre;
+    }
+    
+    
+    // Overrides
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Arbitre) {
+            Arbitre a = (Arbitre) o;
+            if (this.idArbitre == a.getId()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Arbitre ID[%d], %s %s", this.getId(), this.getPrenom(), this.getNom().toUpperCase());
+    }    
 }
--- SAE_E_SPORT/src/entites/Joueur.java
+++ SAE_E_SPORT/src/entites/Joueur.java
@@ -1,52 +1,51 @@
 package entites;
 
-import java.sql.*;
-import java.util.*;
-
 public class Joueur {
-	
-	private int idJoueur;
-	private String pseudo;
-	
-	public Joueur(int idJoueur, String pseudo) {
-		this.idJoueur = idJoueur;
-		this.pseudo = pseudo;
-	}
-	
-	public int getIdJoueur() {
-		return this.idJoueur;
-	}
-	
-	public String getPseudo() {
-		return this.pseudo;
-	}
-	
-	public void setIdJoueur(int idJoueur) {
-		this.idJoueur = idJoueur;
-	}
-	
-	public void setPseudo(String pseudo) {
-		this.pseudo = pseudo;
-	}
-	
-	@Override
-	public boolean equals(Object o) {
-		if(o instanceof Joueur) {
-			Joueur j = (Joueur) o;
-			if (this.idJoueur == j.getIdJoueur()) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		return false;
-	}
-	
-	@Override
-	public String toString() {
-		return this.idJoueur + " - "+this.pseudo;
-	}	
-
-
+    
+    private int idJoueur;
+    private String pseudo;
+    
+    
+    // Constructeur
+    public Joueur(int idJoueur, String pseudo) {
+        this.idJoueur = idJoueur;
+        this.pseudo = pseudo;
+    }
+    
+    
+    // Get
+    public int getId() {
+        return this.idJoueur;
+    }
+    
+    public String getPseudo() {
+        return this.pseudo;
+    }
+    
+    
+    // Set    
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+    
+    
+    // Overrides
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof Joueur) {
+            Joueur j = (Joueur) o;
+            if (this.idJoueur == j.getId()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("Joueur ID[%d], %s", this.getId(), this.getPseudo());
+    }
 }
