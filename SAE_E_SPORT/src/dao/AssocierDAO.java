752327aa567fd8c9e51cebf4d155c3bdc11bb3a3
Zelmine <zelmine31@gmail.com>
2023-12-18 14:54:15 +0100

    selectionArbitre

---
+++ SAE_E_SPORT/src/dao/AssocierDAO.java
@@ -0,0 +1,11 @@
+package dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import modele.Associer;
+import modele.Joueur;
+
+public interface AssocierDAO extends DAO<Associer, Integer>{
+
+}
